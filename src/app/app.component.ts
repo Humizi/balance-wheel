543417a16ac3daf03017cea6b1530c880cfdeb3b
Humizi <humizi307@gmail.com>
2023-09-09 01:28:15 +1000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
+import * as Highcharts from 'highcharts';
+
 import { Component } from '@angular/core';
+import { DEFAULT_SETTINGS } from './default-settings';
+import HighchartsMore from 'highcharts/highcharts-more';
+import HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
+
+HighchartsMore(Highcharts);
+HighchartsSolidGauge(Highcharts);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'balance-wheel';
+  public chart!: Highcharts.Chart;
+
+  public ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = Highcharts.chart('container', {
+      chart: {
+        polar: true,
+        type: 'column',
+      },
+      credits: {
+        enabled: false,
+      },
+      title: {
+        text: undefined,
+      },
+      xAxis: {
+        categories: DEFAULT_SETTINGS.map((item) => item.title || ''),
+        tickmarkPlacement: 'on',
+      },
+      yAxis: {
+        labels: {
+          enabled: false,
+        },
+        gridLineInterpolation: 'circle',
+        tickInterval: 1,
+        min: 0,
+        max: 10,
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0,
+          groupPadding: 0,
+        },
+      },
+      tooltip: {
+        shared: true,
+        pointFormat:
+          '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>',
+      },
+      legend: {
+        enabled: false,
+      },
+      series: [
+        {
+          type: 'column',
+          name: 'Column',
+          data: DEFAULT_SETTINGS.map((item) => item.point),
+          pointPlacement: 'between',
+        },
+      ],
+      responsive: {
+        rules: [
+          {
+            condition: {
+              maxWidth: 500,
+            },
+            chartOptions: {
+              legend: {
+                align: 'center',
+                verticalAlign: 'bottom',
+                layout: 'horizontal',
+              },
+              pane: {
+                size: '70%',
+              },
+            },
+          },
+        ],
+      },
+    });
+  }
 }
---
+++ src/app/default-settings.ts
@@ -0,0 +1,48 @@
+export interface ICategory {
+  title: string | null;
+  point: number | null;
+  color: string | null;
+}
+
+export const DEFAULT_SETTINGS: ICategory[] = [
+  {
+    title: 'Друзья',
+    point: 5,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Здоровье',
+    point: 3,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Работа',
+    point: 7,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Финансы',
+    point: 7,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Отношения',
+    point: 10,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Отдых',
+    point: 2,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Творчество',
+    point: 1,
+    color: '#7CB5EC',
+  },
+  {
+    title: 'Саморазвитие',
+    point: 4,
+    color: '#7CB5EC',
+  },
+];
